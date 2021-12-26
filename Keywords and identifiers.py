467fa2b6686bc1abbf1f372da2d5a1627a70eced
Samadbaig <96692376+Samadbaig@users.noreply.github.com>
2021-12-26 21:50:39 +0530

    Add files via upload
    Keywords and identifiers 
    
    list of rule along with the example for identifiers
    and knowing about the keyword

---
+++ Keywords and identifiers.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Keyword
+
+# ## keyword are reserved word
+
+# ## keywords can not be used as a variable name, function name or any other identifer
+
+# In[1]:
+
+
+import keyword
+
+
+# In[2]:
+
+
+print(keyword.kwlist)
+
+
+# In[3]:
+
+
+print(len(keyword.kwlist))
+
+
+# # identifer
+
+# identifer is  the name given to the entities like class, function,variables etc. in python it help differentiating one entity from another
+
+# # Rules for writing identifers
+
+# # identifers can be a combination of letters in lower case(a to z) or upper case(A to Z) OR Digit(0 to 9) or an underscore(_)
+# an identifer can not  start with digit
+# keywords can't be used as identifer
+identifers can be a combination of letters in lower case(a to z) or upper case(A to Z) OR Digit(0 to 9) or an underscore(_)
+
+an identifer can not start with digit 
+
+keywords can't be used as identifer
+# In[5]:
+
+
+abdul=12 #valid
+
+
+# In[6]:
+
+
+1abdul=13 #invalid
+
+
+# In[7]:
+
+
+abd1_=23 # valid
+
+
+# In[ ]:
+
+
+
+
