87750d1bb9e5e184e701f8d7827119af5d8b4aec
Samadbaig <96692376+Samadbaig@users.noreply.github.com>
2021-12-26 23:16:55 +0530

    Add files via upload
    IN THIS I LEARNED HOW TO COMMENT A SINGLE LINE , MULTIPLE LINE ALONG WITH THE  USE OF INDENTATION

---
+++ Comments and INDENTATION.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # comments
+
+# # comments are the lines that exist in the computer program that are ingored by the complier, interpreter
+
+# # # is the symbol used to comment a line
+
+# # for multiple we  can use '''  ''' or """   """
+
+# In[1]:
+
+
+a='abdul'  #tabdul is a motivation speaker
+
+
+# # PYTHON INDENTATION
+
+# # PYTHON USE TO DEFINE  A BLOCK OF CODE
+
+# In[3]:
+
+
+for i in range(10):
+    print(i)
+
+
+# In[5]:
+
+
+for i in range(10): print(i); print(i & 2) # without indentation but its good idea to use identation
+
+
+# In[ ]:
+
+
+
+
